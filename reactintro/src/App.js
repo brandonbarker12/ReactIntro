66dfeb69bf26fecbdde641911a6e48595c86cd37
brandonbarker12 <brandonjbarker12@gmail.com>
2020-10-29 22:16:07 +0000

    updating

--- reactintro/src/App.js
+++ reactintro/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TaskList from './TaskList';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>To-do List</h1>
-        <input></input>
-        <button>Add Todo</button>
-        <TaskList />
-        <TaskList />
-    </div>
-  );
+class App extends Component {
+    render(){
+        return (
+            <div className="App">
+                <h1>To-do List</h1>
+                <input></input>
+                <button>Add Todo</button>
+                <TaskList />
+                <TaskList />
+            </div>
+        );
+    }
+  
 }
 
 export default App;
